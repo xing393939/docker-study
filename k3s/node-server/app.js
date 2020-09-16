073fdffa08abdca87e00208d196cc45125058bcd
chenxing <chenxing@bookan.com.cn>
2020-09-16 16:01:00 +0800

    1

--- k3s/node-server/app.js
+++ k3s/node-server/app.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Kubia server starting...");
+function getIPAdress() {
+  var interfaces = os.networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
 
+console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("hostname: " + os.hostname() + "\n");
+  response.end("IP: " + getIPAdress() + " host: " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
